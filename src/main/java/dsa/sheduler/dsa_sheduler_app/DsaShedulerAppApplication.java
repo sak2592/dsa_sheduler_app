1d490be8ce35a283bcd397c978937b6bdb140b15
sak <sak.deep.25@gmail.com>
2025-09-15 20:39:50 +0530

    initial commit

---
+++ src/main/java/dsa/sheduler/dsa_sheduler_app/DsaShedulerAppApplication.java
@@ -0,0 +1,13 @@
+package dsa.sheduler.dsa_sheduler_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaShedulerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaShedulerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dsa/sheduler/dsa_sheduler_app/DsaShedulerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dsa.sheduler.dsa_sheduler_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaShedulerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
